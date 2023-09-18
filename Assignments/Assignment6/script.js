356d17bb582fc3483455b6e5c49f5bd8e2fecde7
Mandeep-Gujral <mgujral2@gmail.com>
2023-09-17 22:12:30 -0400

    added assignment 6

---
+++ Assignments/Assignment6/script.js
@@ -0,0 +1,35 @@
+//Column 1 - Show and Hide
+
+const showRocket = () => {
+    document.getElementById("rocket").classList.remove("hide");
+}
+
+const hideRocket = () => {
+    document.getElementById("rocket").classList.add("hide");
+}
+
+// animate
+
+const moveCircle = () => {
+    document.getElementById("circle").classList.add("move-circle");
+}
+
+
+//input stuff
+
+const addComment = () => {
+    const productName = document.getElementById("product-name").value;
+    const commentS = document.getElementById("comment-txt").value;
+    
+    const displayStuff = document.getElementById("comment-container");
+    displayStuff.innerHTML = productName + commentS;
+
+}
+
+window.onload = () => {
+document.getElementById("button-show").onclick = showRocket;
+document.getElementById("button-hide").onclick = hideRocket;
+document.getElementById("dance-button").onclick = moveCircle;
+document.getElementById("add-comment").onclick = addComment;
+}
+
