764d77831be1a3e8045e72e6f28a214287ddee2e
Mandeep-Gujral <mgujral2@gmail.com>
2023-10-20 22:43:41 -0400

    added part3

---
+++ projects/part3/contact.js
@@ -0,0 +1,40 @@
+const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    const result = document.getElementById("result");
+    result.innerHTML = "Please wait...";
+  
+    const form = document.getElementById("contact-form");
+    const formData = new FormData(form);
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+  
+    try {
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: json,
+      });
+  
+      if (response.status === 200) {
+        result.innerHTML = "Thank you for contacting us. We will be in touch shortly!";
+        result.classList.add("success-message");
+        // Hide any existing validation feedback
+        form.querySelectorAll('.empty-feedback, .invalid-feedback').forEach(element => {
+          element.style.display = 'none';
+        });
+      } else {
+        result.innerHTML = "Sorry, your email couldn't be sent.";
+        result.classList.add("error-message");
+      }
+    } catch (error) {
+      console.log(error);
+      result.innerHTML = "Sorry, your email couldn't be sent.";
+      result.classList.add("error-message");
+    }
+  };
+  
+  document.getElementById("contact-form").onsubmit = handleFormSubmit;
+  
