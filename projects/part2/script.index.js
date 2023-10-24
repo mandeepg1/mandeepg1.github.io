1fbeb81df4e9b150fc72fae64dca300f95d091aa
Mandeep-Gujral <mgujral2@gmail.com>
2023-10-23 21:36:52 -0400

    fixing stuff

--- projects/part2/script.index.js
+++
@@ -1,9 +0,0 @@
-
-const toggleNav = () => {
-    document.getElementById("nav-items").classList.toggle("hide-small");
-};
-
-window.onload = () => {
-    document.getElementById("hamburger").onclick = toggleNav;
-    
-}
